a3c2403d8cdd4966d7f3cef46a92908305897104
Jonathan's mac <jonathan@gofrias.com>
2013-10-21 15:29:29 -0400

    first iteration of the rails-production module

--- modules/rails-production.sh
+++ modules/rails-production.sh
@@ -1,6 +1,8 @@
-arch-chroot /mnt pacman -S apache mariadb
-arch-chroot /mnt pacman -S ruby nodejs
+arch-chroot /mnt pacman -S apache mariadb ruby nodejs
+arch-chroot /mnt curl -L get.rvm.io | bash -e stable
+arch-chroot /mnt rvm install 2.0.0
+arch-chroot /mnt rvm use 2.0.0 --default
+arch-chroot /mnt rvm rubygems current
 arch-chroot /mnt gem install rails --no-ri --no-rdoc
 arch-chroot /mnt gem install passenger mysql
-arch-chroot /mnt passenger-isntall-apache2-module
-
+arch-chroot /mnt passenger-install-apache2-module
