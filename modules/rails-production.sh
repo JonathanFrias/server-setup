a3558f0adeaee6ea60018f2a2892e057d8a8f40b
Jonathan's mac <jonathan@gofrias.com>
2013-10-21 17:29:53 -0400

    Cant install rails via chroot. Instead presenting instructions to user

--- modules/rails-production.sh
+++ modules/rails-production.sh
@@ -1,10 +1,14 @@
 arch-chroot /mnt pacman -S apache mariadb ruby nodejs
-
-# reboot and execute the following commands
-# curl -L get.rvm.io | bash -e stable
-# rvm install 2.0.0
-# rvm use 2.0.0 --default
-# rvm rubygems current
-# gem install rails --no-ri --no-rdoc
-# gem install passenger mysql
-# passenger-install-apache2-module
+msg="
+  sudo -u $user curl -L get.rvm.io | bash
+  usermod -aG rvm $user
+  rvm install 2.0.0
+  rvm use 2.0.0 --default
+  rvm rubygems current
+  gem install rails --no-ri --no-rdoc
+  gem install passenger mysql
+  passenger-install-apache2-module
+"
+echo "Please reboot and run the following commands as $user"
+echo $msg > /mnt/home/$user/rails
+echo "These commands have been saved in $user's home directory"
