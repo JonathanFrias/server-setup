bbacf98401d7e45f77563beb8cfcaa1aa72dd31d
Jonathan's mac <jonathan@gofrias.com>
2013-11-06 13:01:26 -0500

    added reminder

--- modules/rails-production.sh
+++ modules/rails-production.sh
@@ -1,11 +1,12 @@
-arch-chroot /mnt pacman -S apache mariadb nodejs python2 python2
+arch-chroot /mnt pacman -S apache mariadb nodejs python python2
 ln -s '/usr/lib/systemd/system/sshd.service' '/etc/systemd/system/multi-user.target.wants/sshd.service'
 export msg="
-  sudo -u $user curl -L get.rvm.io | bash
-  exit # Must logout/login
+  # Connect to Internet
+  dhcpcd
+  curl -L get.rvm.io | bash
+  logout # Must logout/login
   rvm install 2.0.0
   rvm 2.0.0 --default
-  rvm rubygems current
   gem install rails --no-ri --no-rdoc
   gem install passenger mysql
   passenger-install-apache2-module
